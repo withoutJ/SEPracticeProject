b2a1be06ae7e34aa4256c2a2b32982f2b7223cb1
Intiser008 <irahman2-c@my.cityu.edu.hk>
2023-11-22 19:45:44 +0800

    New changes to sport facility (printing available time slots)

--- src/sportfacility/SwimmingPool.java
+++ src/sportfacility/SwimmingPool.java
@@ -1,5 +1,5 @@
 package sportfacility;
-public class SwimmingPool extends SportFacility {
+class SwimmingPool extends SportFacility {
     public SwimmingPool(int openingTime, int closingTime, int bookingFee) {
         super(openingTime, closingTime, bookingFee); // Opening time 9:00, closing time 23:00
     }
--- src/sportfacility/TennisCourt.java
+++ src/sportfacility/TennisCourt.java
@@ -1,5 +1,5 @@
 package sportfacility;
-public class TennisCourt extends SportFacility {
+class TennisCourt extends SportFacility {
     public TennisCourt(int openingTime, int closingTime, int bookingFee) {
         super(openingTime, closingTime, bookingFee); // Opening time 9:00, closing time 23:00
     }
