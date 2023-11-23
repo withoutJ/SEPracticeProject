bd9300275641af8a346f889911c48c3da44b6c43
Intiser008 <irahman2-c@my.cityu.edu.hk>
2023-11-24 07:14:05 +0800

    Added code for main

---
+++ src/main/Main.java
@@ -0,0 +1,175 @@
+package main;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import authentication.*;
+import sportfacility.*;
+import transaction.PayPalPayment;
+import user.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    private static boolean isRunning;
+    private static String username;
+    private static String password;
+    private static User customer;
+    private static List<SportFacility> facilities = new ArrayList<>();
+    
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        facilities.add(new SwimmingPool(9, 20, 10));
+        facilities.add(new BadmintonCourt(9, 23, 20));
+        facilities.add(new BasketballCourt(9, 22, 120));
+        facilities.add(new TennisCourt(9, 22, 50));
+        isRunning = true;
+        while (isRunning){
+            int response = openScreen(scanner);
+            if (response == 1){
+                customer = AuthenticationService.register(username, password);
+            }
+            else if (response == 2){
+                customer = AuthenticationService.login(username, password); // guessing login will use FindUser()
+            }
+            if (customer != null)
+                mainMenu(scanner);
+        }
+        scanner.close(); // Close the scanner only once here
+    }
+
+    private static int openScreen(Scanner scanner){
+        customer = null;
+        System.out.print("Shahbagh Sports Complex\n");
+        System.out.print("---------------------------------------------------------\n");
+        System.out.print("Press 1 to Sign-Up | Press 2 to Sign-In | Press 0 to exit\n");
+        System.out.print("Input: ");
+        int response = -1;
+        while (response != 1 && response != 2 && response != 0){
+            String strResponse = scanner.next();
+            response = Integer.parseInt(strResponse);
+            if (response == 1){
+                System.out.print("Set a username: ");
+                username = scanner.next();
+                System.out.print("Set a password: ");
+                password = scanner.next();
+            }
+            else if (response == 2){
+                System.out.print("Input your username: ");
+                username = scanner.next();
+                System.out.print("Input your password: ");
+                password = scanner.next();
+            }
+            else if (response == 0){
+                System.out.print("Thank you for using our system.\n");
+                isRunning = false;
+            }
+            else{
+                System.out.print("Invalid Token. Try again.\n");
+                System.out.print("Input: ");
+            }
+        }
+        return response;
+    }
+
+    private static void mainMenu(Scanner scanner) {
+        int userInput = -1;
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String dateNtime = now.format(formatter);
+        System.out.print(dateNtime + "\n");
+        while (userInput != 0){
+            System.out.print("Press 1 to make a booking.\n");
+            System.out.print("Press 2 to view bookings.\n");
+            System.out.print("Press 3 to cancel bookings.\n");
+            System.out.print("Press 4 to show available facilities.\n");
+            System.out.print("Press 0 to log out.\n");
+            System.out.print("Input: ");
+            String strUserInput = scanner.next(); // handle exception where input is not a number
+            userInput = Integer.parseInt(strUserInput);
+            switch(userInput){
+                case 1:
+                    makeBooking(scanner);
+                    break;
+                case 2:
+                    viewBooking(scanner);
+                    String strInput = scanner.next();
+                    int input = Integer.parseInt(strInput);
+                    while (input != 0){
+                        if (input != 0){
+                            System.out.print("Invalid token. Try again.\n");
+                            strInput = scanner.next();
+                            input = Integer.parseInt(strInput);
+                        }
+                    }
+                    break;
+                case 3:
+                    cancelBooking(scanner);
+                    break;
+                case 4:
+                    // some logic to print the facilities available
+                    break;
+                case 0:
+                    // log out
+                    break;
+                default:
+                    System.out.print("Invalid token. Try again.\n");
+
+            }
+        }
+    }
+
+    private static void makeBooking(Scanner scanner){
+        System.out.print("Choose a facility (Enter 0 to go to the main menu).\n");
+        System.out.print("1. Swimming\n2. Badminton\n3. Basketball\n4. Tennis\n");
+        System.out.print("Input: ");
+        int facility = -1;
+        boolean bookSuccessful = false;
+        while (facility != 0 && !bookSuccessful){
+            String strFacility = scanner.next();
+            facility = Integer.parseInt(strFacility);
+            switch (facility){
+                case 1: case 2: case 3: case 4:
+                System.out.print("Enter date of booking (DD-MM-YYYY): ");
+                String date = scanner.next();
+                SportFacility chosenFacility = facilities.get(facility - 1);
+                chosenFacility.showAvailableSlots(date);
+                System.out.print("Enter preferred time slot (e.g. if you want to book at 20:00, type 20): ");
+                int time = scanner.nextInt(); // need some method to display available time slots
+                System.out.print("Select a payment method:\n");
+                System.out.print("1. Credit Card\n2. PayPal\n");
+                System.out.print("Input: ");
+                String strPay = scanner.next();
+                int payment = Integer.parseInt(strPay);
+                 // parameter will be changed such that PaymentStrategy is passed as strings of CC/Pl instead of an object
+                bookSuccessful = customer.createBooking(facilities.get(facility - 1), date, time, "new PayPalPayment()");
+                if (bookSuccessful){
+                    System.out.print("Booking successfully create for " + date + " from " + 
+                    Integer.toString(time) + ":00 to " + Integer.toString(time+1) + ":00. Go to main menu to manage your bookings.\n");
+                    System.out.print("------------------------------------------------------------------------------------------------------------------------------------------\n");
+                }
+                break;
+                default:
+                    System.out.print("Invalid token. Try again.\n");
+            }
+            
+        }
+    }
+
+    private static void viewBooking(Scanner scanner){
+        customer.viewBookings();
+        System.out.print("Enter 0 to go to the main menu.\n");
+    }
+
+    private static void cancelBooking(Scanner scanner){
+        boolean cancelled = true;
+        System.out.print("Enter Booking ID to cancel a booking.\n");
+        customer.viewBookings();
+        String bookID = scanner.next();
+        customer.cancelBooking(bookID);
+        if (cancelled)
+            System.out.print("Booking cancelled successfully.\n");
+        else
+            System.out.print("Booking does not exist, try again.\n");
+    }
+}
